a6c9353321db648410ecce998d658709d454c3ac
dmsvk0217 <dmsvk0217@gmail.com>
2025-04-09 00:17:16 +0900

    refactor: Feedback DTO ApiProperty 추가

--- src/module/feedback/dto/request/feedback.create-request.ts
+++ src/module/feedback/dto/request/feedback.create-request.ts
@@ -1,4 +1,4 @@
-import { OmitType } from "@nestjs/swagger";
+import { ApiProperty, OmitType } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsDate, IsEnum, IsNotEmpty, IsString } from "class-validator";
 import { Feedback } from "src/module/feedback/entities/feedback.entity";
@@ -11,23 +11,28 @@ export class FeedbackCreateRequest extends OmitType(Feedback, [
   "updatedAt",
   "deletedAt",
 ]) {
+  @ApiProperty({ example: FeedbackType.DAILY, description: "피드벡 타입" })
   @IsNotEmpty()
   @IsEnum(FeedbackType)
   type: FeedbackType;
 
-  @Type(() => Date)
+  @ApiProperty({ example: "2025-01-01", description: "피드백 날짜" })
   @IsNotEmpty()
   @IsDate()
+  @Type(() => Date)
   date: Date;
 
+  @ApiProperty({ example: "this is good point", description: "좋았던 점" })
   @IsNotEmpty()
   @IsString()
   goodPoint: string;
 
+  @ApiProperty({ example: "this is badpoint", description: "나빴던 점" })
   @IsNotEmpty()
   @IsString()
   badPoint: string;
 
+  @ApiProperty({ example: "this is comment", description: "기타 사항" })
   @IsNotEmpty()
   @IsString()
   comment: string;
--- src/module/feedback/dto/response/feedback.response.ts
+++ src/module/feedback/dto/response/feedback.response.ts
@@ -9,28 +9,27 @@ export class FeedbackResponse extends IntersectionType(
   OmitType(Feedback, ["user"] as const),
   BaseResponseDto
 ) {
-  @Expose()
   @ApiProperty({ type: () => UserResponse })
+  @Expose()
   user: UserResponse;
 
+  @ApiProperty({ example: FeedbackType.DAILY, description: "피드벡 타입" })
   @Expose()
   type: FeedbackType;
 
+  @ApiProperty({ example: "2025-01-01", description: "피드백 날짜" })
   @Expose()
   date: Date;
 
+  @ApiProperty({ example: "this is good point", description: "좋았던 점" })
   @Expose()
   goodPoint: string;
 
+  @ApiProperty({ example: "this is badpoint", description: "나빴던 점" })
   @Expose()
   badPoint: string;
 
+  @ApiProperty({ example: "this is comment", description: "기타 사항" })
   @Expose()
   comment: string;
-
-  @Expose()
-  name: string;
-
-  @Expose()
-  color: string;
 }
