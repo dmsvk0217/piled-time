a9eb42234fdaac67cd56e6bb63be40abeab2c83f
dmsvk0217 <dmsvk0217@gmail.com>
2025-04-11 00:02:00 +0900

    feat: JwtAuthGuard user조회하여 request에 할당

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,9 +1,14 @@
 import { ExecutionContext, Injectable, Logger } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { UserService } from "../../module/user/user.service";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
   private logger = new Logger(JwtAuthGuard.name);
+
+  constructor(private readonly userService: UserService) {
+    super();
+  }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -11,28 +16,21 @@ export class JwtAuthGuard extends AuthGuard("jwt") {
 
     try {
       const result = (await super.canActivate(context)) as boolean;
+      const { email } = request.user;
 
-      const user = request.user as any;
+      const user = await this.userService.findByEmail(email);
+      request.user = user;
 
       this.logger.log(
-        `[✅ JWT 인증 성공]
-➡️ ${method} ${originalUrl}
-👤 userId: ${user?.id ?? "N/A"}, email: ${user?.email ?? "N/A"}
-🌐 IP: ${ip}
-📱 UA: ${headers["user-agent"]}
-`
+        `✅ JWT 인증 성공 | ${method} ${originalUrl} | userId=${user?.id ?? "N/A"}, email=${user?.email ?? "N/A"}, ip=${ip}`
       );
 
       return result;
     } catch (err) {
       this.logger.warn(
-        `[❌ JWT 인증 실패]
-➡️ ${method} ${originalUrl}
-🌐 IP: ${ip}
-📱 UA: ${headers["user-agent"]}
-💥 이유: ${err?.message ?? "Unknown error"}
-`
+        `❌ JWT 인증 실패 | ${method} ${originalUrl} | ip=${ip} | reason=${err?.message ?? "Unknown error"}`
       );
+
       throw err;
     }
   }
