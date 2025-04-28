d1d0bee5919191e8a66cc25721f85a253ec0e5e5
dmsvk0217 <dmsvk0217@gmail.com>
2025-04-28 21:55:25 +0900

    [client] feat: add route /*

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,6 +22,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/demo" element={<DemoPage />} />
+        <Route path="/*" element={<HomePage />} />
       </Route>
     </Routes>
   );
