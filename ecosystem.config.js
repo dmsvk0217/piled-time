6a4cd823ee47e3e2a850ba2a13d980f9d4d6db99
dmsvk0217 <dmsvk0217@gmail.com>
2025-04-28 21:06:55 +0900

    config: pm2 설정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "piled-server",
-      script: "dist/main.js",
+      script: "./dist/main.js",
       cwd: "./server",
       env: {
         NODE_ENV: "production",
@@ -10,6 +10,14 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      exec_mode: "fork",
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      merge_logs: true,
+      time: true,
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      log_type: "json",
+      log_file: "./logs/combined.log",
     },
   ],
 };
