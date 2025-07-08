15d8d62810b9269341f94f291cee34c5c3cfce01
dmsvk0217 <dmsvk0217@gmail.com>
2025-07-08 17:46:01 +0900

    test

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
 
   const port = process.env.PORT || 4000;
   await app.listen(port);
-  console.log(`🚀 Application is running on: port ${port}`);
+  console.log(`🚀  Application is running on: port ${port}`);
 }
 bootstrap();
