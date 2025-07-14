2e8a580195b6215d5b93378b023362eca4988bcc
dmsvk0217 <dmsvk0217@gmail.com>
2025-07-14 15:03:35 +0900

    conf: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# .env.production 파일에서 환경변수 로딩
+source .env.production
+
 echo "🚀 Pulling latest images from Docker Hub..."
+
+docker pull ${DOCKER_USER}/react-nginx-app
+docker pull ${DOCKER_USER}/nest-server
+
+docker compose -f docker-compose.ec2.yml down
 docker compose -f docker-compose.ec2.yml pull
 
 echo "🔁 Restarting containers..."
-docker compose -f docker-compose.ec2.yml down
 docker compose -f docker-compose.ec2.yml up -d --force-recreate
 
 echo "✅ Deployment complete."
