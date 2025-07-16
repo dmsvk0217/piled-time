0afb46187f96c5639eafaa7f81e042c4a2bc9851
dmsvk0217 <dmsvk0217@gmail.com>
2025-07-16 15:30:39 +0900

    [client] feat: actionsAPI CRUD

---
+++ client/src/api/actionApi.ts
@@ -0,0 +1,32 @@
+import { Action } from "@/types/action";
+import api from "./axios";
+
+export const fetchActions = async (date: string): Promise<Action[]> => {
+  const res = await api.get<Action[]>("/api/actions", { params: { date } });
+  return res.data;
+};
+
+export const createAction = async (
+  todoId: number,
+  startAt: string,
+  duration: number
+): Promise<Action> => {
+  const res = await api.post<Action>("/api/actions", {
+    todoId,
+    startAt,
+    duration,
+  });
+  return res.data;
+};
+
+export const updateAction = async (
+  id: number,
+  data: Partial<{ startAt: string; duration: number }>
+): Promise<Action> => {
+  const res = await api.patch<Action>(`/api/actions/${id}`, data);
+  return res.data;
+};
+
+export const deleteAction = async (id: number): Promise<void> => {
+  await api.delete(`/api/actions/${id}`);
+};
