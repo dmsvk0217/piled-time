36f10cf623ac224de3be580aaf07c9e53d2b5fb9
dmsvk0217 <dmsvk0217@gmail.com>
2025-07-16 15:30:15 +0900

    [client] userCategory

---
+++ client/src/hooks/useCategory.ts
@@ -0,0 +1,28 @@
+import { fetchCategories } from "@/api/categoryApi";
+import { Category } from "@/types/category";
+import { useEffect, useState } from "react";
+
+export function useCategory() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const data = await fetchCategories();
+      setCategories(data);
+      setError(null);
+    } catch (err) {
+      console.error(err);
+      setError("카테고리 데이터를 불러오는 데 실패했습니다.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { categories, error, isLoading, fetchData };
+}
