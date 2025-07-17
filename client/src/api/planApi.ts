5fcee38f2d1dd2aec409c05b62cf468468d61983
dmsvk0217 <dmsvk0217@gmail.com>
2025-07-17 15:11:52 +0900

    [client] feat: planApi CRUD

---
+++ client/src/api/planApi.ts
@@ -0,0 +1,32 @@
+import { Plan } from "@/types/plan";
+import api from "./axios";
+
+export const fetchPlans = async (date: string): Promise<Plan[]> => {
+  const res = await api.get<Plan[]>("/api/plans", { params: { date } });
+  return res.data;
+};
+
+export const createPlan = async (
+  todoId: number,
+  startAt: string,
+  duration: number
+): Promise<Plan> => {
+  const res = await api.post<Plan>("/api/plans", {
+    todoId,
+    startAt,
+    duration,
+  });
+  return res.data;
+};
+
+export const updatePlan = async (
+  id: number,
+  data: Partial<{ startAt: string; duration: number }>
+): Promise<Plan> => {
+  const res = await api.patch<Plan>(`/api/plans/${id}`, data);
+  return res.data;
+};
+
+export const deletePlan = async (id: number): Promise<void> => {
+  await api.delete(`/api/plans/${id}`);
+};
