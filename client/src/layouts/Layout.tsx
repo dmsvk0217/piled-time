0638d9a63d125765767da6483bef6a487637fd6d
dmsvk0217 <dmsvk0217@gmail.com>
2025-08-07 19:53:36 +0900

    [client] fix: Layout 디자인 개선

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -21,24 +21,24 @@ export default function Layout() {
       <nav className="relative bg-white shadow px-6 flex items-center justify-between">
         {/* 로고 (좌측 고정) */}
         <Link to="/" className="flex items-center gap-2">
-          <img src={logo} alt="Piled Time" className="h-20 w-auto" />
+          <img src={logo} alt="Piled Time" className="h-16 w-auto" />
         </Link>
 
         {/* 메뉴 (가운데 정렬) */}
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-20 text-lg font-medium">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-20">
           <Link
             to="/"
-            className="text-gray-700 hover:text-black tracking-wide hover:font-semibold transition">
+            className="text-gray-700  font-normal hover:text-black tracking-wide hover:font-semibold transition">
             플래닝
           </Link>
           <Link
             to="/stats/weekly"
-            className="text-gray-700 hover:text-black tracking-wide hover:font-semibold transition">
+            className="text-gray-700 font-normal hover:text-black tracking-wide hover:font-semibold transition">
             주간 통계
           </Link>
           <Link
             to="/stats/monthly"
-            className="text-gray-700 hover:text-black tracking-wide hover:font-semibold transition">
+            className="text-gray-700 font-normal hover:text-black tracking-wide hover:font-semibold transition">
             월간 통계
           </Link>
         </div>
